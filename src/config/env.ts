f50691c8d266d50e889fa044b74d3850dfbacad2
ASDlikeS <penetresing_test@tutamail.com>
2025-03-08 18:27:36 +0300

    fix: rename PORT and DOMAIN_NAME

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,8 +4,8 @@ dotenv.config();
 
 export const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN as string;
 export const ADMIN_ID = process.env.ADMIN_ID;
-export const PORT = Number(process.env.PORT);
-export const DOMAIN_NAME = process.env.DOMAIN_NAME;
+export const PORT = process.env.PORT;
+export const DOMAIN_NAME = process.env.DOMAIN;
 
 if (!TELEGRAM_TOKEN) {
     throw new Error('Telegram token is not defined');
@@ -14,7 +14,7 @@ if (!ADMIN_ID) {
     throw new Error('Admin id is not defined');
 }
 if (!PORT) {
-    throw new Error('Admin id is not defined');
+    throw new Error('Port is not defined');
 }
 if (!DOMAIN_NAME) {
     throw new Error('Domain name is not defined');
